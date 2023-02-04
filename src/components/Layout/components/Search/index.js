ba697b7383013fc46712e84da405edd35b6f5f46
Long Tran <tranxuanlonga555@gmail.com>
2023-02-04 16:41:24 +0700

    create scroll menu

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -50,44 +50,47 @@ function Search() {
     }
   };
 
-  return (
-    <HeadlessTippy
-      interactive
-      visible={showResult && searchResult.length > 0}
-      render={(attrs) => (
-        <div className={cx("search-result")} tabIndex="-1" {...attrs}>
-          <PopperWrapper>
-            <h4 className={cx("search-title")}>Accounts</h4>
-            {searchResult.map((result) => (
-              <AccountItem key={result.id} data={result} />
-            ))}
-          </PopperWrapper>
-        </div>
-      )}
-      onClickOutside={handleHideResult}
-    >
-      <div className={cx("search")}>
-        <input
-          ref={inputRef}
-          placeholder="Search accounts and videos"
-          spellCheck={false}
-          onChange={handleChange}
-          value={searchValue}
-          onFocus={() => setShowResult(true)}
-        />
-        {!loading && !!searchValue && (
-          <button className={cx("clear")} onClick={handleClear}>
-            <FontAwesomeIcon icon={faCircleXmark} />
+  return ( 
+    // Using a wrapper <div> or <span> tag around the reference element solves this by creating a new parentNode context. 
+     <div> 
+      <HeadlessTippy
+        interactive
+        visible={showResult && searchResult.length > 0}
+        render={(attrs) => (
+          <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+            <PopperWrapper>
+              <h4 className={cx("search-title")}>Accounts</h4>
+              {searchResult.map((result) => (
+                <AccountItem key={result.id} data={result} />
+              ))}
+            </PopperWrapper>
+          </div>
+        )}
+        onClickOutside={handleHideResult}
+      >
+        <div className={cx("search")}>
+          <input
+            ref={inputRef}
+            placeholder="Search accounts and videos"
+            spellCheck={false}
+            onChange={handleChange}
+            value={searchValue}
+            onFocus={() => setShowResult(true)}
+          />
+          {!loading && !!searchValue && (
+            <button className={cx("clear")} onClick={handleClear}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+          )}
+          {loading && (
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+          )}
+          <button className={cx("search-btn")} onMouseDown={e => e.preventDefault()}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
           </button>
-        )}
-        {loading && (
-          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
-        )}
-        <button className={cx("search-btn")} onMouseDown={e => e.preventDefault()}>
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </button>
-      </div>
-    </HeadlessTippy>
+        </div>
+      </HeadlessTippy>
+    </div>
   );
 }
 
